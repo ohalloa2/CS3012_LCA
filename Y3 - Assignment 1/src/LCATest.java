edce75b8804720a538cd0585e37ce5fc927157e4
Aoife O'Halloran <ohalloa2@tcd.ie>
2018-09-26 17:47:00 +0100

    Possible tests added

--- Y3 - Assignment 1/src/LCATest.java
+++ Y3 - Assignment 1/src/LCATest.java
@@ -8,5 +8,39 @@ public class LCATest {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test 
+	public void testForEmptyTree(){
+	}
+	
 
+	@Test
+	public void testForTreeSize1(){
+		
+	}
+	
+	@Test
+	public void testForTreeSize9(){
+		
+	}
+	
+	@Test
+	public void testForTreeSize21(){
+		
+	}
+	
+	@Test
+	public void testForNonExitingNodes(){
+		
+	}
+	
+	@Test
+	public void testWhenOneNodeIsAncestorOfOther(){
+		
+	}
+	
+	@Test
+	public void testForALlSameNode(){
+		
+	}
 }
