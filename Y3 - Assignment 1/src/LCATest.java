c796620e681eb2c03863311094217a69e88f1418
Aoife O'Halloran <ohalloa2@tcd.ie>
2018-09-30 18:25:22 +0100

    Test for tree size 9

--- Y3 - Assignment 1/src/LCATest.java
+++ Y3 - Assignment 1/src/LCATest.java
@@ -23,6 +23,21 @@ public class LCATest {
 	@Test
 	public void testForTreeSize9(){
 		
+		LCA treeSize9 = new LCA();
+		treeSize9.root = new Node(1);
+		treeSize9.root.left = new Node(2);
+		treeSize9.root.right = new Node(3);
+		treeSize9.root.left.left = new Node(4);
+		treeSize9.root.left.right = new Node(5);
+		treeSize9.root.right.left = new Node(6);
+		treeSize9.root.right.right = new Node(7);
+		treeSize9.root.left.left.left = new Node(8);
+		treeSize9.root.left.left.right = new Node(9);
+		
+		assertEquals("LCA(1,3", 1, treeSize9.findLCA(1,3));
+		assertEquals("LCA(4,5)", 2,treeSize9.findLCA(4, 5));
+		assertEquals("LCA(8,9)", 4,treeSize9.findLCA(8, 9));
+		assertEquals("LCA(6,9)", 3, treeSize9.findLCA(6, 9));	
 	}
 	
 	@Test
