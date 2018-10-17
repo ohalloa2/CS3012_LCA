f76f6760b93514ab79f1476fc73faf244c6ac3b5
Aoife O'Halloran <ohalloa2@tcd.ie>
2018-10-17 16:19:08 +0100

    Possible Dag Tests added

--- Y3 - Assignment 1/src/DAGTest.java
+++ Y3 - Assignment 1/src/DAGTest.java
@@ -1,3 +1,25 @@
+import org.junit.Test;
+
 public class DAGTest {
+	
+	@Test 
+	public void testE()
+	{
+		
+	}
+	
+	@Test
+	public void testV()
+	{
+		
+	}
+	
+	@Test 
+	public void testLCA ()
+	{
+		
+	}
+	
+	
 	
 }
