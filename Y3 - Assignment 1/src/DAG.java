66da95118f8adfea67515e5cbcd1a80169252bd4
Aoife O'Halloran <ohalloa2@tcd.ie>
2018-10-17 16:11:01 +0100

    Possible Classes Added to DAG

--- Y3 - Assignment 1/src/DAG.java
+++ Y3 - Assignment 1/src/DAG.java
@@ -19,7 +19,30 @@ class Node
 
 public class DAG {
 	
+	public int V()
+	{
+		return 0 ;
+	}
 	
-
+	public int E()
+	{
+		return 0;
+	}
+	
+	public void addEdge(int v, int w)
+	{
+		
+	}
+	
+	public int LCA(int v, int w)
+	{
+	return 0;	
+	}
+	
+	public ArrayList<Integer>bfs(int s)
+	{
+		
+	}
+	
 }
 
